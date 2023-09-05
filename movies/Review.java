0f499b8da24272df8b8c4fe12d3ef2f3ba5d3527
Sohan Nannra <49798755+snannra@users.noreply.github.com>
2023-09-04 17:37:52 -0700

    Add files via upload

---
+++ movies/Review.java
@@ -0,0 +1,23 @@
+package dev.sohan.movies;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.bson.types.ObjectId;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+    public Review(String body) {
+        this.body = body;
+    }
+}
